2331d56b48726e707d517bfa5fe7d30c46f6963a
OssamaAourfi <aourfidev@gmail.com>
2024-02-19 18:05:56 +0100

    addIventory

---
+++ GlobalSuperStore/src/main/java/com/ltp/GlobalSuperStore/Constants.java
@@ -0,0 +1,5 @@
+package com.ltp.GlobalSuperStore;
+
+public class Constants {
+    public static  final String[] Categories = new String[]{"Furniture","Animaley","Shop"};
+}
--- GlobalSuperStore/src/main/java/com/ltp/GlobalSuperStore/StoreController.java
+++ GlobalSuperStore/src/main/java/com/ltp/GlobalSuperStore/StoreController.java
@@ -1,12 +1,18 @@
 package com.ltp.GlobalSuperStore;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class StoreController {
     @GetMapping("/")
-    public String getForm(){
+    public String getForm(Model model){
+        model.addAttribute("categories",Constants.Categories);
         return "form";
+    }
+    @GetMapping("/inventory")
+    public String getInventory(){
+        return "inventory";
     }
 }
