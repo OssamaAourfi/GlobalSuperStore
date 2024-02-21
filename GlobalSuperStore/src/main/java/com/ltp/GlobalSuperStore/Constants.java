a58957428628f4e12b82216a693ab628b67777c3
OssamaAourfi <aourfidev@gmail.com>
2024-02-21 17:20:10 +0100

    addDate

--- GlobalSuperStore/src/main/java/com/ltp/GlobalSuperStore/Constants.java
+++ GlobalSuperStore/src/main/java/com/ltp/GlobalSuperStore/Constants.java
@@ -3,4 +3,7 @@ package com.ltp.GlobalSuperStore;
 public class Constants {
     public static final int NOT_FOUND = -10000;
     public static  final String[] Categories = new String[]{"Furniture","Animaley","Shop"};
+    public static final String SUCCESS_STATUS = "success";
+    public static final String FAILED_STATUS = "failed";
+
 }
--- GlobalSuperStore/src/main/java/com/ltp/GlobalSuperStore/StoreController.java
+++ GlobalSuperStore/src/main/java/com/ltp/GlobalSuperStore/StoreController.java
@@ -5,9 +5,12 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @Controller
 public class StoreController {
@@ -20,15 +23,18 @@ public class StoreController {
         return "form";
     }
     @PostMapping("/submitItem")
-    public String handleSubmit(Item item){
+    public String handleSubmit(Item item, RedirectAttributes redirectAttributes){
         int index = getIndexFromId(item.getId()) ;
+        String status = Constants.SUCCESS_STATUS;
         if(index == Constants.NOT_FOUND){
             items.add(item);
         }
-        else{
+        else if(within5Days(item.getDate(),items.get(index).getDate())){
             items.set(index,item);
+        }else{
+            status= Constants.FAILED_STATUS;
         }
-
+        redirectAttributes.addFlashAttribute("status",status);
         return "redirect:/inventory";
     }
     @GetMapping("/inventory")
@@ -42,5 +48,10 @@ public class StoreController {
                 return i;
         }
         return  Constants.NOT_FOUND;
+    }
+    public boolean within5Days(Date newDate, Date oldDate){
+        long diff = Math.abs(newDate.getTime() - oldDate.getTime());
+        return(int)(TimeUnit.MILLISECONDS.toDays(diff))<=5;
+
     }
 }
